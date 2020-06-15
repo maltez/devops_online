0ae833645cbc5f3045eb9604485ab860212bfeee
Nick Lototskiy <nicklototskiy@inner.directory>
2020-06-15 20:58:23 +0300

    Add lab 06

---
+++ examples/lab-05/app.js
@@ -0,0 +1,19 @@
+const { createServer } = require('http');
+
+const server = createServer((req, res) => {
+    console.log(`Request from [${req.method}]:${req.url}`);
+    if(req.url === "/err"){
+        return;
+    }
+
+    res.statusCode = 200;
+    res.write(`Hello Nick from ${req.url}`);
+    res.end();
+});
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server listen on ${HOST}:${PORT}`);
+});
