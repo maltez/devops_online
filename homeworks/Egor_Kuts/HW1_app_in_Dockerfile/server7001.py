5ab953eea09da59d4981a051e430920efffa49d8
Egor Kuts <ekuts@redpointpositioning.com>
2020-06-21 01:09:06 +0300

    HW1 and HW2

---
+++ homeworks/Egor_Kuts/HW1_app_in_Dockerfile/server7001.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+import sys
+
+PORT = 7001
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
---
+++ homeworks/Egor_Kuts/README.md
@@ -0,0 +1,6 @@
+Egor Kuts folder with homeworks
+
+Contacts:
+- [mail](mailto:liraykh@gmail.com)
+- [github](https://github.com/LirayKH)
+- [DockerHub](https://hub.docker.com/u/kutsegor)
