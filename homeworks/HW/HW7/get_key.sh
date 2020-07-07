ed5a7300f5833e8453ff4f0d6ca450393e6900a9
SA <53128011+Mihailov-SA@users.noreply.github.com>
2020-07-07 12:48:02 +0300

    Update get_key.sh

--- homeworks/HW/HW7/get_key.sh
+++ homeworks/HW/HW7/get_key.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#export VAULT_ADDR=http://127.0.0.1:8200
-#openssl rand -base64 32
 ./vault_cli login -address=http://127.0.0.1:8200 -no-print token=root
 ./vault_cli kv put -address=http://127.0.0.1:8200 secret/key0 key0-1=$(openssl rand -base64 32) > /dev/null
-
 ./vault_cli kv get -address=http://127.0.0.1:8200 -field="key0-1" secret/key0
