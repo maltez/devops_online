5ee0a674f4ccbdbec534ac0a3bd1eb72a5e68a9d
Volodymyr Poddubetskyi <66464693+poddubetskyi@users.noreply.github.com>
2020-07-14 00:34:06 +0300

    Доработано домашнее задание 1 с учетом замечаний преподавателя
    1) Заметки из файла Dockerfile вынесены в файл справки README.md
    
    2) Переработано ошибочное использование значения параметра PORT,
    объявленного в коде файла Dockerfile как переменная окружения,
    в качестве аргумента к директиве EXPOSE.
    Для обеспечения возможности настройки параметров PORT и HOST
    в сценарии Dockerfile предусмотрены одноименные аргументы,
    а также конструкции определения для них значений по умолчанию.
    Определены переменные окружения PORT и HOST (ENV),
    значения которых определяются значениями одноименных
    параметров сборки (ARG).

--- homeworks/poddubetskyi/hw_01_docker_basics/hello_app/hello.sh
+++ homeworks/poddubetskyi/hw_01_docker_basics/hello_app/hello.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
-# Источники кода "сервера": https://fabianlee.org/2016/09/26/ubuntu-simulating-a-web-server-using-netcat/
-# Закрыть соедининение после отправки ответа: https://serverfault.com/a/880289
-# Обработка сигналов в сценарии оболочки: http://linuxcommand.org/lc3_wss0150.php
+[ -z "${HOST}" ] && HOST=0.0.0.0
+[ -z "${PORT}" ] && PORT=8880 
 trap "echo Terminating... ; exit" SIGHUP SIGINT SIGTERM
 while true; do { 
   echo -e "HTTP/1.1 200 OK\r\n$(date)\r\n\r\n<h1>Docker basics hello app</h>" \
   "<h3>Host: $(hostname); Date: $(date "+%0Y.%0m.%0d %0H:%0M:%0S")</h3>" ;
-  } |  nc -q 1 -vl -p ${PORT} 0.0.0.0 ;
+  } |  nc -q 1 -vl -p ${PORT} ${HOST} ;
 done
